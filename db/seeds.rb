566b0e113ba244faba3f3d3eb3d41db1d956679a
Elrae Sheppard <elrae@rjbtechnology.com>
2019-06-13 13:35:17 +0800

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Tobio", password: "password")
+User.create(username: "Shoyu", password: "password")
+User.create(username: "Norman", password: "password")
+User.create(username: "Emma", password: "password")
+User.create(username: "Ray", password: "password")
+User.create(username: "Victor", password: "password")
